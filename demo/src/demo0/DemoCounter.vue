bf26e66caf4ee45081d6b1550de9bd79044c9843
Constantine Cher <Konstantin.Cherepanov@infotecs.ru>
2024-08-15 10:51:25 +0300

    add part 15082024

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="counter">
+        <button @click="decrement"> - </button>
+
+        <span> Current value: {{ count }} </span>
+
+        <button @click="increment"> + </button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+const increment = () => {
+  count.value = count.value + 1
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+
+  button {
+    margin: 0 10px;
+    font-size: 1.5rem;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    border: none;
+    background: #dd0000;
+    color: white;
+    cursor: pointer;
+    display: flex;    
+    justify-content: center;
+    align-items: center;
+    
+    &:first-of-type {
+      align-items: baseline;
+    }
+
+    &:hover {
+      background: #ff0000;
+    }
+
+    &:active {
+      background: #ff00ff;
+    }
+  }
+}
+</style>
+
---
+++ frontend/src/router/routes.js
@@ -0,0 +1,27 @@
+export default [
+	{
+		path: '/',
+		name: 'HomeView',
+		component: () => import('../views/HomeView.vue'),
+		children: [
+            {
+				path: '/:id',
+				name: 'TaskView',
+				component: () => import('../views/TaskView.vue'),
+				meta: { layout: 'AppLayoutMain' }
+			},
+			{
+				path: '/tasks/create',
+				name: 'TaskCreate',
+				component: () => import('../views/TaskCreate.vue'),
+				meta: { layout: 'AppLayoutMain' }
+			},
+			{
+				path: '/tasks/edit/:id',
+				name: 'TaskEdit',
+				component: () => import('../views/TaskEdit.vue'),
+				meta: { layout: 'AppLayoutMain' }
+			}
+        ]
+	},
+]
